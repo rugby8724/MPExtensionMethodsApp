be022a7efc8c5be86862a9b105d2a84394984bf7
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-09-12 11:19:01 -0500

    Add project files.

---
+++ MPExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.WriteLine(message);
+                output = Console.ReadLine();
+            }
+            return output;
+        }
+
+        public static int RequestInt(this string message, int minValue, int maxValue) 
+        {
+            return message.RequestInt(true, minValue, maxValue);
+        }
+
+
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        private static int RequestInt(this string message, bool useMinMax, int minValue = 0, int maxValue = 0) 
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool isInValidRange = true;
+
+            while (isValidInt == false || isInValidRange == false)
+            {
+                Console.WriteLine(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMinMax == true)
+                {
+                  isInValidRange = (output >= minValue && output <= maxValue);
+                }
+            }
+            return output;
+
+        }
+    }
+}
---
+++ MPExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using MPExtensionMethods;
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "What is your first name: ".RequestString();
+
+person.LastName = "What is your last name: ".RequestString();
+
+person.Age = "What is your age: ".RequestInt(0, 130);
+
+
+Console.ReadLine();
